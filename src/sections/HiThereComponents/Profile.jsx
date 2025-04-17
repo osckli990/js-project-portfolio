7e6c66ede418d1fe3c158b38516f5cd5529e708b
Oscar Liljefors <oscar.lf@live.se>
2025-04-18 00:38:04 +0200

    trying to fix netlify

--- src/sections/HiThereComponents/Profile.jsx
+++ src/sections/HiThereComponents/Profile.jsx
@@ -5,7 +5,7 @@ import profileImage from "../../assets/cropped_profile.png";
 export const Profile = () => {
   return (
     <img
-      className="my-4 mx-auto"
+      className="my-4 mx-auto h-154" //min height!!
       src={profileImage}
       alt="Profile image"
       aria-label="Profile image"
